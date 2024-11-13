c7c1c0a851cd73fd4557a960149239f6ad0cb601
rdrlaguna <rdr.laguna@gmail.com>
2024-11-13 07:19:20 +0100

    Set up canvas congiguration

---
+++ rain/script.js
@@ -0,0 +1,9 @@
+
+document.addEventListener('load', function() {
+    // Canvas configuration
+    const canvas = this.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 1000;
+    canvas.heigth = 1500;
+    
+})
