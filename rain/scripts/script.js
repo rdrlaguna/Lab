aacc4aeee5f70d36161d0bef525dcd64eea2b9c7
rdrlaguna <rdr.laguna@gmail.com>
2024-11-13 07:32:18 +0100

    Update particle position

--- rain/scripts/script.js
+++ rain/scripts/script.js
@@ -12,12 +12,24 @@ document.addEventListener('load', function() {
 
     class Particle {
         constructor() {
+            // Particle position
             this.x = Math.random() * canvas.width;
             this.y = 0;
-
+            // Particle attributes
             this.speed = 0;
-            this.velocity = Math.random() * 0.5;
+            this.velocity = Math.random() * 3.5;
             this.size = Math.random() * 10 + 1;
+        }
+
+        update() {
+            // Change particle position
+            this.y += this.velocity;
+            // Reposition  particle on top of canvas
+            if (this.y >= canvas.heigth) {
+                this.y = 0;
+                this.x = Math.random() * canvas.width;
+            }
+
         }
     }
     
