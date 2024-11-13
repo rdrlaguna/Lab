87aa8c9ea163f0bd2785096122ae8e009e6968b3
rdrlaguna <rdr.laguna@gmail.com>
2024-11-13 07:28:22 +0100

    Define Particle class

--- rain/scripts/script.js
+++ rain/scripts/script.js
@@ -5,5 +5,20 @@ document.addEventListener('load', function() {
     const ctx = canvas.getContext('2d');
     canvas.width = 1000;
     canvas.heigth = 1500;
+
+    // Hold all particles
+    let particles = [];
+    const totalParticles = 1000;
+
+    class Particle {
+        constructor() {
+            this.x = Math.random() * canvas.width;
+            this.y = 0;
+
+            this.speed = 0;
+            this.velocity = Math.random() * 0.5;
+            this.size = Math.random() * 10 + 1;
+        }
+    }
     
-})
+});
